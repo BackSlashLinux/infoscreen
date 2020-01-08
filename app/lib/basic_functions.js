dd0f3ebaf6107df934de3116d5be3a066cdc784c
Kumar Priyansh <30593201+luciferreeves@users.noreplygithub.com>
2020-01-08 10:25:39 +0530

    updated js functions

--- app/lib/basic_functions.js
+++ app/lib/basic_functions.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    
+    $('#moveUp').click(function(){
+        $('.staticInfoContainer').toggleClass('movedUp')
+        if ($('#moveUp').text() == 'Show Detailed System Information') {
+            $('#moveUp').text('Hide Detailed System Information')
+        } else {
+            $('#moveUp').text('Show Detailed System Information')
+        }
+    })
 })
