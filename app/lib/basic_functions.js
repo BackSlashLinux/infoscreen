ddcc279222789e05ca2996f84315600092838077
Kumar Priyansh <kumarpriyansh@icloud.com>
2020-01-08 13:11:05 +0530

    Updated System Info Module and Basic Functions

--- app/lib/basic_functions.js
+++ app/lib/basic_functions.js
@@ -1,10 +1,28 @@
 $(document).ready(function() {
+
+    changeBackground()
+
+    $('#changebg').click(function() {
+        changeBackground()
+    })
+
+    function changeBackground() {
+        var min= 1
+        var max = 19;
+
+        var randomnumber = (Math.floor(Math.random() * (max - min + 1)) + min)
+
+        $('.fullbg').css('background-image', 'url("./assets/BG'+randomnumber+'.jpeg")')
+    }
+
     $('#moveUp').click(function(){
         $('.staticInfoContainer').toggleClass('movedUp')
+        $('.detailedViewContainer').slideToggle()
         if ($('#moveUp').text() == 'Show Detailed System Information') {
             $('#moveUp').text('Hide Detailed System Information')
         } else {
             $('#moveUp').text('Show Detailed System Information')
         }
     })
+
 })
---
+++ app/lib/systemInfoModule.js
@@ -0,0 +1,92 @@
+const si = require('systeminformation');
+
+$(document).ready(function () {
+
+    // CPU Graph Plotting Algorithm
+
+    var dataPoints = [];
+    var xValue = 0;
+
+    var chart = new CanvasJS.Chart("cpuLoadContainer", {
+        height: 119,
+        interactivityEnabled: false,
+        backgroundColor: "transparent",
+        axisX:{
+            labelFontColor: "transparent",
+            lineColor: "white",
+            tickColor: "transparent",
+        },
+        axisX2:{
+            lineColor: "transparent",
+        },
+        axisY:{
+            labelFontFamily: "Lato",
+            labelFontColor: "white",
+            labelFontSize: 12,
+            suffix: "%",  
+            minimum: 0,
+            maximum: 100,
+        },
+        data: [{
+            type: "splineArea",
+            color:"white",
+            markerSize: 0,
+            dataPoints: dataPoints
+        }]
+    });
+
+    updateData();
+
+    function addData(cpuLoad) {
+        xValue += 5;
+        if (dataPoints.length > 150) {
+            dataPoints.shift()
+        }
+        dataPoints.push({ x: xValue, y: parseInt(cpuLoad) });
+
+        $('#cpuLoadValue').text(`CPU Load: ${cpuLoad}%`)
+        
+        chart.render();
+        setTimeout(updateData, 500);
+    }
+
+    function updateData() {
+
+        si.currentLoad()
+            .then(data => addData(Math.round(data.currentload)))
+            // .catch(error => console.error(error));
+    }
+
+
+    // CPU Information Algorithm
+
+    si.cpu()
+    .then((data) => {
+        $('#cpuInformation').text(data.manufacturer + ' ' + data.brand + ' ' + data.speed + 'GHz')
+        // console.log(data)
+    })
+
+
+    // Add RAM Total
+
+    si.mem()
+    .then((data) => {
+        $('#ramInformation').text((data.total)/(1024*1024*1024) + 'GB Total RAM')
+    })
+
+    window.setInterval(() => {
+        si.mem()
+        .then((data) => {
+            $('#ramLoadValue').text(((data.free)/(1024*1024*1024)).toFixed(2) + ' GB Free')
+            var used = ((data.total - data.free)/data.total) * 100
+            $('#ram').css('width', used+'%')
+        })
+    }, 500)
+
+    si.graphics()
+    .then((data) => {
+        $('#graphicsData').text(`${data.controllers[0].vram} MB ${data.controllers[0].model}`)
+        console.log(data)
+    })
+
+})
